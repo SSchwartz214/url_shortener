ec42f7fc3e09a00e4a7a64f055fe130b08fdcbc3
Seth Schwartz <sschwartz214@gmail.com>
2018-12-17 13:36:55 -0700

    Adds method to generate unique short urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,12 +1,17 @@
 class Url < ApplicationRecord
   validates_presence_of :original
-  after_create :generate_short, :scrape_title
+  after_create :assign_short, :scrape_title
+  validates_uniqueness_of :short
   scope :top_100, -> { order(clicks: :desc).limit(100)}
   
-  def generate_short
-    self.short = self.id.to_s(36)
+  def assign_short
+    generated_short = self.id.to_s(36)
 
-    self.save
+    if Url.exists?(short: generated_short)
+      assign_short
+    else
+      update_attribute(:short, generated_short)
+    end
   end
 
   def scrape_title
